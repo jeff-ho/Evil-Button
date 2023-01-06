b634d99f94bb6585d037b36526f76b5cb4899cd7
Jeffrey <jeff.ho@hotmail.com>
2023-01-06 16:26:55 -0500

    Fixed Window coordinates going off screen

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [coord, setCoord] = useState({top:450,left:800})
+  const [coord, setCoord] = useState({ top: null, left: null });
 
   const randomHeight = () => {
-    return Math.floor(Math.random() * 500)
-  }
+    return Math.floor(Math.random() * window.innerHeight);
+  };
 
   const randomWidth = () => {
-    return Math.floor(Math.random() * 1000)
-  }
-  
+    return Math.floor(Math.random() * window.innerWidth);
+  };
+
   const moveButton = () => {
-      setCoord({...coord, top:randomHeight(), left:randomWidth() })
-  }
+    let height = randomHeight();
+    let width = randomWidth();
+
+    if (width > 800) {
+      width = width - 250;
+    }
+
+    if (height > 920) height = height - 400;
+
+    setCoord({ ...coord, top: height, left: width });
+    console.log(randomHeight(), randomWidth());
+  };
 
   return (
     <div className="App">
       <button
-      style={{top: coord.top, left: coord.left}}
-      onMouseEnter={() => moveButton()}
-      >Click Me 
+        style={{ top: coord.top, left: coord.left }}
+        onMouseEnter={() => moveButton()}
+      >
+        Click Me
       </button>
     </div>
   );
